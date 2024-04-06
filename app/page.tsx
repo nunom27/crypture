3234cc0493e2a95ff81d3b57118d1211a4d793ff
Nuno Miguel <nmpf.2005@gmail.com>
2024-04-06 13:25:33 +0100

    feat: add sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import CoinRates from "@/components/CoinRates";
+import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-«
-    </main>
+    <Sidebar>
+      <div>Content</div>
+    </Sidebar>
   );
 }
