b27f7d431f8996113db0175505e16170a2736967
Heitor Fernandes <heitoraraujofernandes03@gmail.com>
2024-04-06 17:34:52 +0100

    Fix: name

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <Sidebar>
-      <div className="text-xltitle leading-tight"><span className="bg-gradient-to-r from-purple-200 to-gold inline-block text-transparent bg-clip-text font-bold">Good morning,</span> <br></br> Heitor</div>
+      <div className="text-xltitle leading-tight"><span className="bg-gradient-to-r from-purple-200 to-gold inline-block text-transparent bg-clip-text font-bold">Good morning,</span> <br></br> Code Sailors</div>
     </Sidebar>
   );
 }
