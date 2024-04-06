3277593f03e9ded788c0d4c21206f3bf7b6e9cb5
Heitor Fernandes <heitoraraujofernandes03@gmail.com>
2024-04-06 17:23:31 +0100

    feat: sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <Sidebar>
-      <div>Content</div>
+      <div className="text-xltitle leading-tight"><span className="bg-gradient-to-r from-purple-200 to-gold inline-block text-transparent bg-clip-text font-bold">Good morning,</span> <br></br> Heitor</div>
     </Sidebar>
   );
 }
