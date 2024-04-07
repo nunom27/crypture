b762a322ffbb144b2dabd9ac7349bf759d02e4b7
Heitor Fernandes <heitoraraujofernandes03@gmail.com>
2024-04-07 08:52:04 +0100

    Feat: Pop Up

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -2,12 +2,14 @@
 import { SetStateAction, useEffect, useRef, useState } from 'react';
 import WalletInfo1 from './WalletInfo1';
 import WalletInfo2 from './WalletInfo2';
+import PopUp from './PopUp';
 
 const ACCOUNT_WALLET = 'accountwallet';
 const BOT_WALLET = 'botwallet';
 
 function Wallet() {
   const [selectedWallet, setSelectedWallet] = useState('accountwallet');
+  const [popUpOpened, setPopUpOpened] = useState(false)
 
   return (
     <div className="flex flex-col justify-start items-start gap-10 w-full max-w-[1600px] mx-auto">
@@ -22,18 +24,21 @@ function Wallet() {
               <span className='text-text font-bold'>Account Wallet</span>
           </div>
           <div 
-              onClick={() => setSelectedWallet(BOT_WALLET)}
-              style={{backgroundColor: selectedWallet === BOT_WALLET ? '#901BC7' : '#F3F4F6',
-                      color: selectedWallet === BOT_WALLET ? '#FFFFFF' : '#000000'}}
-              className='w-auto h-[50px] px-[16px] rounded-[25px] flex flex-col items-center justify-center text-text'
+            onClick={() => setSelectedWallet(BOT_WALLET)}
+            style={{backgroundColor: selectedWallet === BOT_WALLET ? '#901BC7' : '#F3F4F6',
+                    color: selectedWallet === BOT_WALLET ? '#FFFFFF' : '#000000'}}
+            className='w-auto h-[50px] px-[16px] rounded-[25px] flex flex-col items-center justify-center text-text'
           >
-              <span className='text-text font-bold'>Bot Wallet</span>
+            <span className='text-text font-bold'>Bot Wallet</span>
           </div>
       </div>
       <div className="flex flex-col justify-start items-start gap-10 w-full max-w-[1600px] mx-auto">
-        {/* Your other code here */}
         {selectedWallet === ACCOUNT_WALLET && <WalletInfo1 />}
         {selectedWallet === BOT_WALLET && <WalletInfo2 />}
+      </div>
+      <button onClick={() => setPopUpOpened(true)}>Test</button>
+      <div className="flex flex-col justify-start items-start gap-10 w-full max-w-[1600px] mx-auto">
+        {popUpOpened === true && <PopUp popUpOpened={popUpOpened} setPopUpOpened={setPopUpOpened} />}
       </div>
     </div>
   );
