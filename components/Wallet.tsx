8a94fb9e9549acff631a729f8193f0d684dcc923
Heitor Fernandes <heitoraraujofernandes03@gmail.com>
2024-04-07 07:32:44 +0100

    Feat: Different wallets

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -1,85 +1,44 @@
 "use client";
-import { Sanchez } from 'next/font/google';
-// TradingViewWidget.jsx
 import { SetStateAction, useEffect, useRef, useState } from 'react';
-import {
-    CurrencyDollarIcon,
-    CircleStackIcon,
-  } from '@heroicons/react/24/solid'
+import WalletInfo1 from './WalletInfo1';
+import WalletInfo2 from './WalletInfo2';
 
+const ACCOUNT_WALLET = 'accountwallet';
+const BOT_WALLET = 'botwallet';
 
 function Wallet() {
-    const [selectedWallet, setSelectedWallet] = useState('');
-    return(
-        <div className="flex flex-col justify-start items-start gap-10 w-full max-w-[1600px] mx-auto">
-             <div className="text-mtitle leading-tight font-extrabold gap-[15px]">Wallet</div>
-             <div className="flex flex-row gap-[10px]">
-                <div 
-                    onClick={() => setSelectedWallet('accountwallet')}
-                    style={{backgroundColor: selectedWallet === 'accountwallet' ? '#901BC7' : '#F3F4F6',
-                            color: selectedWallet === 'accountwallet' ? '#FFFFFF' : '#000000'}}
-                    className='w-auto h-[50px] px-[16px] rounded-[25px] flex flex-col items-center justify-center text-text'
-                >
-                    <span className='text-text font-bold'>Account Wallet</span>
-                </div>
-                <div 
-                    onClick={() => setSelectedWallet('botwallet')}
-                    style={{backgroundColor: selectedWallet === 'botwallet' ? '#901BC7' : '#F3F4F6',
-                            color: selectedWallet === 'botwallet' ? '#FFFFFF' : '#000000'}}
-                    className='w-auto h-[50px] px-[16px] rounded-[25px] flex flex-col items-center justify-center text-text'
-                >
-                    <span className='text-text font-bold'>Bot Wallet</span>
-                </div>
-            </div>
+  const [selectedWallet, setSelectedWallet] = useState('accountwallet');
 
-            <div className='flex flex-row w-full gap-[10px]'>
-                    <div className="flex flex-col gap-[5px] w-[50%] h-auto rounded-[15px] bg-gray-100 p-[20px] text-black">
-                            <div className='flex flex-row mt-[8px]'>
-                                <CurrencyDollarIcon className='w-[20px] h-[20px] text-black mr-[6px]'></CurrencyDollarIcon>
-                                <span className="text-black text-text font-bold">ACCOUNT BALANCE</span>
-                            </div>
-                        <span className='text-ltitle font-extrabold'>assets$</span>
-                        <span className='text-text font-medium mt-[5px] opacity-50'>This is the combined value in USD of all your cryptocurrency.</span>
-                    </div>
-                    <div className="flex flex-col gap-[5px] w-[50%] h-auto rounded-[15px] bg-gray-100 p-[20px] text-black">
-                        <div className='flex flex-row mt-[8px]'>
-                            <CircleStackIcon className='w-[20px] h-[20px] text-black mr-[6px]'></CircleStackIcon>
-                            <span className="text-black text-text font-bold">ACCOUNT ASSETS</span>
-                        </div>
-                        <div className='flex flex-row justify-start items-center mt-[10px]'>
-                            <div className='bg-black size-[50px] rounded-[50%] mr-[15px]'></div>
-                            <div>
-                                <span className='font-bold text-headline'>value</span>
-                                <span className='font-medium text-headline'>abbreviation</span>
-                                <br></br>
-                                <span className='font-medium text-text opacity-50'>coin name</span>
-                            </div>
-                        </div>
-                        <div className='flex flex-row justify-start items-center mt-[10px]'>
-                            <div className='bg-black size-[50px] rounded-[50%] mr-[15px]'></div>
-                            <div>
-                                <span className='font-bold text-headline'>value</span>
-                                <span className='font-medium text-headline'>abbreviation</span>
-                                <br></br>
-                                <span className='font-medium text-text opacity-50'>coin name</span>
-                            </div>
-                        </div>
-                        <div className='flex flex-row justify-start items-center mt-[10px]'>
-                            <div className='bg-black size-[50px] rounded-[50%] mr-[15px]'></div>
-                            <div>
-                                <span className='font-bold text-headline'>value</span>
-                                <span className='font-medium text-headline'>abbreviation</span>
-                                <br></br>
-                                <span className='font-medium text-text opacity-50'>coin name</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-             <span className='text-header font-bold'>Transaction History</span>
-        </div>
-        );
+  return (
+    <div className="flex flex-col justify-start items-start gap-10 w-full max-w-[1600px] mx-auto">
+      <div className="text-mtitle leading-tight font-extrabold gap-[15px]">Wallet</div>
+        <div className="flex flex-row gap-[10px]">
+          <div 
+              onClick={() => setSelectedWallet(ACCOUNT_WALLET)}
+              style={{backgroundColor: selectedWallet === ACCOUNT_WALLET ? '#901BC7' : '#F3F4F6',
+                      color: selectedWallet === ACCOUNT_WALLET ? '#FFFFFF' : '#000000'}}
+              className='w-auto h-[50px] px-[16px] rounded-[25px] flex flex-col items-center justify-center text-text'
+          >
+              <span className='text-text font-bold'>Account Wallet</span>
+          </div>
+          <div 
+              onClick={() => setSelectedWallet(BOT_WALLET)}
+              style={{backgroundColor: selectedWallet === BOT_WALLET ? '#901BC7' : '#F3F4F6',
+                      color: selectedWallet === BOT_WALLET ? '#FFFFFF' : '#000000'}}
+              className='w-auto h-[50px] px-[16px] rounded-[25px] flex flex-col items-center justify-center text-text'
+          >
+              <span className='text-text font-bold'>Bot Wallet</span>
+          </div>
+      </div>
+      <div className="flex flex-col justify-start items-start gap-10 w-full max-w-[1600px] mx-auto">
+        {/* Your other code here */}
+        {selectedWallet === ACCOUNT_WALLET && <WalletInfo1 />}
+        {selectedWallet === BOT_WALLET && <WalletInfo2 />}
+      </div>
+    </div>
+  );
 }
+
 export default Wallet;
 
 
---
+++ components/WalletInfo1.tsx
@@ -0,0 +1,58 @@
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { CurrencyDollarIcon, CircleStackIcon } from '@heroicons/react/24/solid'
+
+function WalletInfo1() {
+  return(
+    <div className="flex flex-col justify-start items-start gap-10 w-full max-w-[1600px] mx-auto">
+        <div className='flex flex-row w-full gap-[10px]'>
+                <div className="flex flex-col gap-[5px] w-[50%] h-auto rounded-[15px] bg-gray-100 p-[20px] text-black">
+                        <div className='flex flex-row mt-[8px]'>
+                            <CurrencyDollarIcon className='w-[20px] h-[20px] text-black mr-[6px]'></CurrencyDollarIcon>
+                            <span className="text-black text-text font-bold">ACCOUNT BALANCE</span>
+                        </div>
+                    <span className='text-ltitle font-extrabold'>assets$</span>
+                    <span className='text-text font-medium mt-[5px] opacity-50'>This is the combined value in USD of all your cryptocurrency.</span>
+                </div>
+                <div className="flex flex-col gap-[5px] w-[50%] h-auto rounded-[15px] bg-gray-100 p-[20px] text-black">
+                    <div className='flex flex-row mt-[8px]'>
+                        <CircleStackIcon className='w-[20px] h-[20px] text-black mr-[6px]'></CircleStackIcon>
+                        <span className="text-black text-text font-bold">ACCOUNT ASSETS</span>
+                    </div>
+                    <div className='flex flex-row justify-start items-center mt-[10px]'>
+                        <div className='bg-black size-[50px] rounded-[50%] mr-[15px]'></div>
+                        <div>
+                            <span className='font-bold text-headline'>value</span>
+                            <span className='font-medium text-headline'>abbreviation</span>
+                            <br></br>
+                            <span className='font-medium text-text opacity-50'>coin name</span>
+                        </div>
+                    </div>
+                    <div className='flex flex-row justify-start items-center mt-[10px]'>
+                        <div className='bg-black size-[50px] rounded-[50%] mr-[15px]'></div>
+                        <div>
+                            <span className='font-bold text-headline'>value</span>
+                            <span className='font-medium text-headline'>abbreviation</span>
+                            <br></br>
+                            <span className='font-medium text-text opacity-50'>coin name</span>
+                        </div>
+                    </div>
+                    <div className='flex flex-row justify-start items-center mt-[10px]'>
+                        <div className='bg-black size-[50px] rounded-[50%] mr-[15px]'></div>
+                        <div>
+                            <span className='font-bold text-headline'>value</span>
+                            <span className='font-medium text-headline'>abbreviation</span>
+                            <br></br>
+                            <span className='font-medium text-text opacity-50'>coin name</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+         <span className='text-header font-bold'>Transaction History</span>
+    </div>
+    );
+};
+
+export default WalletInfo1;
