51791befefe676c5e8b01a54a1fa5a2b7497afc5
Heitor Fernandes <heitoraraujofernandes03@gmail.com>
2024-04-07 02:28:48 +0100

    .

--- components/Bot.tsx
+++ components/Bot.tsx
@@ -3,36 +3,35 @@
 import { SetStateAction, useEffect, useRef, useState } from 'react';
 
 function Bot() {
-    const [selectedOption, setSelectedOption] = useState(null);
-  
-    const select = (option: string | SetStateAction<null>) => {
-      setSelectedOption(option);
-    };
-  
-    return (
-      <div className="flex flex-col justify-start items-start gap-10 w-full max-w-[1600px] mx-auto">
-        <div className="text-mtitle leading-tight font-extrabold">Crypture Bot</div>
-        <form>
-          <span className='text-header font-extrabold'>Choose a cryptocurrency</span>
-          <div className="flex">
-            <div
-              id="option1"
-              className={`border p-4 m-2 cursor-pointer ${selectedOption === 'option1' ? 'bg-blue-500 text-white' : ''}`}
-              onClick={() => select('option1')}
-            >
-              Option 1
-            </div>
-            <div
-              id="option2"
-              className={`border p-4 m-2 cursor-pointer ${selectedOption === 'option2' ? 'bg-blue-500 text-white' : ''}`}
-              onClick={() => select('option2')}
-            >
-              Option 2
-            </div>
-          </div>
-        </form>
-      </div>
-    );
-  }
+
+  const [selectedCrypto, setSelectedCrypto] = useState('');
+
+  return (
+    <div className="flex flex-col justify-start items-start gap-10 w-full max-w-[1600px] mx-auto">
+      <div className="text-mtitle leading-tight font-extrabold">Crypture Bot</div>
+      <form>
+        <span className='text-header font-extrabold'>Choose a cryptocurrency</span>
+        <div 
+        onClick={() => setSelectedCrypto('crypto1')}
+        style={{backgroundColor: selectedCrypto === 'crypto1' ? 'lightgray' : 'white'}}
+        >
+          <label>Crypto 1</label>
+        </div>
+        <div 
+        onClick={() => setSelectedCrypto('crypto2')}
+        style={{backgroundColor: selectedCrypto === 'crypto2' ? 'lightgray' : 'white'}}
+        >
+          <label>Crypto 2</label>
+        </div>
+        <div 
+        onClick={() => setSelectedCrypto('crypto3')}
+        style={{backgroundColor: selectedCrypto === 'crypto3' ? 'lightgray' : 'white'}}
+        >
+          <label>Crypto 3</label>
+        </div>
+      </form>
+    </div>
+  );
+}
 
 export default Bot;
