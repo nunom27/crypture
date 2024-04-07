c86b9c5d1e6961b5b4132abe69c413069578bb27
a106801 <a106801@alunos.uminho.pt>
2024-04-07 05:32:47 +0100

    .

--- components/Bot.tsx
+++ components/Bot.tsx
@@ -4,6 +4,9 @@ import { SetStateAction, useEffect, useRef, useState } from 'react';
 import {
     SparklesIcon,
   } from '@heroicons/react/24/solid'
+  import {
+    ArrowRightIcon,
+  } from '@heroicons/react/24/outline'
 
 function Bot() {
 
@@ -14,14 +17,31 @@ function Bot() {
     return (
         <div className="flex flex-col justify-start items-start gap-10 w-full max-w-[1600px] mx-auto">
             <div className="text-mtitle leading-tight font-extrabold">Crypture Bot</div>
-            <div className="flex flex-col gap-[5px] w-[100%] h-auto rounded-[15px] bg-gray-100 p-[20px] text-black">
-                    <div className='flex flex-row mt-[8px]'>
-                        <SparklesIcon className='w-[20px] h-[20px] text-black mr-[6px]'></SparklesIcon>
-                        <span className="text-black text-text font-extrabold">BOT BALANCE</span>
-                    </div>
-                <span className='text-ltitle font-extrabold'>assets$</span>
-                <span className='text-text font-medium mt-[5px] opacity-50'>This is the capital that's currently in the hands of Crypture Bot.</span>
+            <div className='flex flex-row w-full gap-[10px]'>
+                <div className="flex flex-col gap-[5px] w-[50%] h-auto rounded-[15px] bg-gray-100 p-[20px] text-black">
+                        <div className='flex flex-row mt-[8px]'>
+                            <SparklesIcon className='w-[20px] h-[20px] text-black mr-[6px]'></SparklesIcon>
+                            <span className="text-black text-text font-extrabold">BOT BALANCE</span>
+                        </div>
+                    <span className='text-ltitle font-extrabold'>assets$</span>
+                    <span className='text-text font-medium mt-[5px] opacity-50'>This is the capital that's currently in the hands of Crypture Bot.</span>
+                </div>
+                <div className="flex flex-col gap-[5px] w-[50%] h-auto rounded-[15px] bg-gray-100 p-[20px] text-black">
+                        <div className='flex flex-row mt-[8px]'>
+                            <SparklesIcon className='w-[20px] h-[20px] text-black mr-[6px]'></SparklesIcon>
+                            <span className="text-black text-text font-extrabold">BOT COIN</span>
+                        </div>
+                        <div className='flex flex-row'>
+                            <span className='text-ltitle font-extrabold'>value</span>
+                            <span className='text-ltitle font-medium'>abbreviation</span>
+                        </div>
+                    <span className='text-text font-medium mt-[5px] opacity-50'>This is the coin Crypture Bot is currently handling.</span>
+                </div>
             </div>
+            <div className='flex flex-row mt-[8px]'>
+                            <ArrowRightIcon className='w-[20px] h-[20px] text-purple-200 mr-[6px]'></ArrowRightIcon>
+                            <span className="text-purple-200 text-text font-medium">Check your wallet</span>
+                        </div>
             <div className='flex flex-col justify-left gap-[2px]'>
                 <div className='bg-black h-[1.5px] opacity-10 my-[10px]'></div>
                 <div className='text-text font-extrabold opacity-50'>REPLACE THIS INVESTMENT</div>
--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -15,7 +15,7 @@ function Wallet() {
                 <div className="flex flex-col gap-[5px] w-[50%] h-auto rounded-[15px] bg-gray-100 p-[20px] text-black">
                         <div className='flex flex-row mt-[8px]'>
                           <CurrencyDollarIcon className='w-[20px] h-[20px] text-black mr-[6px]'></CurrencyDollarIcon>
-                          <span className="text-black text-text font-extrabold">YOUR BALANCE</span>
+                          <span className="text-black text-text font-extrabold">ACCOUNT BALANCE</span>
                         </div>
                     <span className='text-ltitle font-extrabold'>assets$</span>
                     <span className='text-text font-medium mt-[5px] opacity-50'>This is the combined value in USD of all your cryptocurrency.</span>
@@ -23,7 +23,7 @@ function Wallet() {
                 <div className="flex flex-col gap-[5px] w-[50%] h-auto rounded-[15px] bg-gray-100 p-[20px] text-black">
                     <div className='flex flex-row mt-[8px]'>
                         <CircleStackIcon className='w-[20px] h-[20px] text-black mr-[6px]'></CircleStackIcon>
-                        <span className="text-black text-text font-extrabold">YOUR ASSETS</span>
+                        <span className="text-black text-text font-extrabold">ACCOUNT ASSETS</span>
                     </div>
                     <div className='flex flex-row justify-start items-center mt-[10px]'>
                         <div className='bg-black size-[50px] rounded-[50%] mr-[15px]'></div>
